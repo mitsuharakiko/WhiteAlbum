7b065267ebbf79500ce0d53bc02baddd9dbedbf7
Hydra <banshee1115@163.com>
2017-05-23 10:59:16 +0800

    handlebars引用，express中静态资源的引用

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
+
+// 使用handlebars
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 8888);
 
+// 加载静态资源
+app.use(express.static(path.resolve(__dirname , './public')));
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/about', function (req, res) {
+	res.render('about');
+});
+
 // 404 
-app.use(function (req, res) {
-	res.type('text/plain');
+app.use(function (req, res, next) {
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 
 // 500
-app.use(function (req, res) {
-	res.type('text/plain');
+app.use(function (err, req, res, next) {
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 app.listen(app.get('port'), function () {
